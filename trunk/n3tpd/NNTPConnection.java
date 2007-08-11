3acec437be701201647c3eb156bf4156b7919d8b
klinsebaer <klinsebaer@f3743b7d-5b37-0410-9af5-94621080eead>
2007-08-11 17:06:09 +0000

    Methods getProperty(..) renamed to get(..)

--- trunk/n3tpd/NNTPConnection.java
+++ trunk/n3tpd/NNTPConnection.java
@@ -36,12 +36,12 @@ public class NNTPConnection extends Thread
   public static final String MESSAGE_ID_PATTERN = "<[^>]+>";
   
   private boolean            debug              
-    = Boolean.parseBoolean(Config.getInstance().getProperty("n3tpd.debug", "false"));
+    = Boolean.parseBoolean(Config.getInstance().get("n3tpd.debug", "false"));
   private Socket             socket;
   private boolean            exit               = false;
   private BufferedWriter     out;
   private BufferedReader     in;
-  private File               articleDir         = new File(Config.getInstance().getProperty("n3tpd.datadir"));
+  private File               articleDir         = new File(Config.getInstance().get("n3tpd.datadir"));
   private Article            currentArticle     = null;
   private Group              currentGroup       = null;
 
@@ -350,7 +350,7 @@ public class NNTPConnection extends Thread
 
     try
     {
-      printStatus(200, Config.getInstance().getProperty("n3tpd.hostname")
+      printStatus(200, Config.getInstance().get("n3tpd.hostname")
           + " " + Main.VERSION + " news server ready - (posting ok).");
     }
     catch (IOException e1)
